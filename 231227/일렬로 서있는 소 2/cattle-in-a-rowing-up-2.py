8999c0da5a70849587bc7171c032e90890c6074f
Masonlee122 <146700742+Masonlee122@users.noreply.github.com>
2023-12-27 01:00:22 +0900

    Title: 일렬로 서있는 소 2, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231227/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,12 @@
+n = int(input())
+data = list(map(int,input().split()))
+
+
+cnt = 0
+for i in range(len(data)-2):
+    for j in range(i+1,len(data)-1):
+        for k in range(j+1,len(data)):
+            if data[i] <= data[j] and data[j] <= data[k]:
+                cnt+=1
+
+print(cnt)
