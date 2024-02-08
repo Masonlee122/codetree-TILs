5d376ef4a2844dbab8655d572387204f6a023249
Masonlee122 <146700742+Masonlee122@users.noreply.github.com>
2024-02-08 23:33:31 +0900

    Title: 두 사람, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240208/두 사람/two-person.py
+++ 240208/두 사람/two-person.py
@@ -1,7 +1,7 @@
 a =list(input().split())
 b =list(input().split())
 
-if (a[0] >= '19' and a[1] =='M') or (b[0] >= '19' and b[1] =='M'):
+if (int(a[0]) >= 19 and a[1] =='M') or (int(b[0]) >= 19 and b[1] =='M'):
     print(1)
 else:
     print(0)
