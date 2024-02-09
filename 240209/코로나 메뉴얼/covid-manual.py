273716dbd21c77674546b2f93e936e93d0b73c5c
Masonlee122 <146700742+Masonlee122@users.noreply.github.com>
2024-02-09 21:10:43 +0900

    Title: 코로나 메뉴얼, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240209/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,16 @@
+person_1 = list(input().split())
+person_2 = list(input().split())
+person_3 = list(input().split())
+
+lst = [person_1,person_2,person_3]
+cnt = 0
+
+for i in lst:
+    if i[0] == 'Y' and int(i[1]) > 37:
+        cnt+=1
+    
+
+if cnt >=2 :
+    print('E')
+else:
+    print('N')
