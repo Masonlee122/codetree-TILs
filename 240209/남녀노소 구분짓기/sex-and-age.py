2302bd517db7edef881177a47b8d1cf9f659f7a2
Masonlee122 <146700742+Masonlee122@users.noreply.github.com>
2024-02-09 20:53:30 +0900

    Title: 남녀노소 구분짓기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240209/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,15 @@
+gen = int(input())
+age = int(input())
+
+if gen == 0 :
+    if age >= 19:
+        pritn('MAN')
+    else:
+        print('BOY')
+    
+
+else :
+    if age >= 19:
+        pritn('WOMAN')
+    else:
+        print('GIRL')
