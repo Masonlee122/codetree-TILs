2fb8a769619b3be0691ff0cae718ba304c016a57
Masonlee122 <146700742+Masonlee122@users.noreply.github.com>
2024-08-18 22:00:21 +0900

    Title: 007, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240818/007/007.py
@@ -0,0 +1,16 @@
+### 객체 
+### 객체란 여러 특성을 가진 하나의 대상을 일컫는다.
+### 여러 특징 미리 정의하고 하나의 객체 변수에 담으며 이를 Class이다.
+
+#### 클래스 기본 틀 ###
+# class Student:
+#     def __init__(self,kor,eng,math):
+#         self.kor = kor
+#         self.eng = enf
+#         self.math == math
+
+code , point , time = input().split()
+
+print(f"secret code : {code}" )
+print(f"meeting point : {point}" )
+print(f"time : {time}" )
